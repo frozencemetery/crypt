856a97915e230b0c133e4313819b59511fd80654
Ian Denhardt <ian@zenhack.net>
2018-05-13 20:49:03 -0400

    Pretty-print parsed AST.
    much easier to inspect this way.

--- Main.hs
+++ Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
-import Crypt.Parser (file)
-import Text.Parsec (runParser)
+import Crypt.Parser     (file)
+import Text.Parsec      (runParser)
+import Text.Show.Pretty (ppShow)
 
 main :: IO ()
 main = do
     contents <- getContents
-    print (runParser file () "<stdin>" contents)
+    putStrLn $ ppShow $ runParser file () "<stdin>" contents
